dc267e65f8cccfd3e687ac4d6a8239318f9f5a01
Raul <raulnsoares2007@gmail.com>
2026-04-08 01:52:32 -0300

    implementando spring security e entidades

---
+++ src/main/java/com/raulagendador/usuario/infrastructure/repository/UsuarioRepository.java
@@ -0,0 +1,18 @@
+package com.raulagendador.usuario.infrastructure.repository;
+
+import com.raulagendador.usuario.infrastructure.entity.Usuario;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    boolean existsByEmail(String email);
+
+    Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
+}
